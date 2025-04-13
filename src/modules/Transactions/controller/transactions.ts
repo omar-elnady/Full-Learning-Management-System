f81a47d639680420e7cb20ad41ce513ebf80843d
omar-elnady <omar.elnadey99@gmail.com>
2025-04-13 17:51:13 +0200

    Add Transaction list function

--- src/modules/Transactions/controller/transactions.ts
+++ src/modules/Transactions/controller/transactions.ts
@@ -1,111 +1,131 @@
+import Stripe from "stripe";
 
-import Stripe from 'stripe'
+import dotenv from "dotenv";
+import { NextFunction, Response, Request } from "express";
+import Course from "../../../../DB/models/courseModel";
+import Transaction from "../../../../DB/models/transactionModel";
+import UserCourseProgress from "../../../../DB/models/userCourseProgressModel";
 
-import dotenv from 'dotenv'
-import { NextFunction, Response, Request } from 'express'
-import Course from '../../../../DB/models/courseModel'
-import Transaction from '../../../../DB/models/transactionModel'
-import UserCourseProgress from '../../../../DB/models/userCourseProgressModel'
-
-dotenv.config()
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "")
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
 
 if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error('Missing STRIPE_SECRET_KEY is required but not fount in env variable')
+  throw new Error(
+    "Missing STRIPE_SECRET_KEY is required but not fount in env variable"
+  );
 }
 
+export const listTransactions = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.query;
+
+  try {
+    const transactions = userId
+      ? await Transaction.query("userId").eq(userId).exec()
+      : await Transaction.scan().exec();
+
+    res.json({
+      message: "Transactions Retrieved Successfully",
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error Retrieved Transactions ", error });
+  }
+};
+
 export const createStripePaymentIntent = async (
-    req: Request,
-    res: Response
-  ): Promise<void> => {
-    let { amount } = req.body;
-  
-    if (!amount || amount <= 0) {
-      amount = 50;
-    }
-  
-    try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: "usd",
-        automatic_payment_methods: {
-          enabled: true,
-          allow_redirects: "never",
-        },
-      });
-  
-      res.json({
-        message: "",
-        data: {
-          clientSecret: paymentIntent.client_secret,
-        },
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Error creating stripe payment intent", error });
-    }
-  };
-  
-  export const createTransaction = async (
-    req: Request,
-    res: Response
-  ): Promise<void> => {
-    const { userId, courseId, transactionId, amount, paymentProvider } = req.body;
-  
-    
-    try {
-      // 1. get course info
-      const course = await Course.get(courseId);
-  
-      // 2. create transaction record
-      const newTransaction = new Transaction({
-        dateTime: new Date().toISOString(),
-        userId,
-        courseId,
-        transactionId,
-        amount,
-        paymentProvider,
-      });
-      await newTransaction.save();
-      
-      // 3. create initial course progress
-      const initialProgress = new UserCourseProgress({
-        userId,
-        courseId,
-        enrollmentDate: new Date().toISOString(),
-        overallProgress: 0,
-        sections: course.sections.map((section: any) => ({
-          sectionId: section.sectionId,
-          chapters: section.chapters.map((chapter: any) => ({
-            chapterId: chapter.chapterId,
-            completed: false,
-          })),
+  req: Request,
+  res: Response
+): Promise<void> => {
+  let { amount } = req.body;
+
+  if (!amount || amount <= 0) {
+    amount = 50;
+  }
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: "never",
+      },
+    });
+
+    res.json({
+      message: "",
+      data: {
+        clientSecret: paymentIntent.client_secret,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating stripe payment intent", error });
+  }
+};
+
+export const createTransaction = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, courseId, transactionId, amount, paymentProvider } = req.body;
+
+  try {
+    // 1. get course info
+    const course = await Course.get(courseId);
+
+    // 2. create transaction record
+    const newTransaction = new Transaction({
+      dateTime: new Date().toISOString(),
+      userId,
+      courseId,
+      transactionId,
+      amount,
+      paymentProvider,
+    });
+    await newTransaction.save();
+
+    // 3. create initial course progress
+    const initialProgress = new UserCourseProgress({
+      userId,
+      courseId,
+      enrollmentDate: new Date().toISOString(),
+      overallProgress: 0,
+      sections: course.sections.map((section: any) => ({
+        sectionId: section.sectionId,
+        chapters: section.chapters.map((chapter: any) => ({
+          chapterId: chapter.chapterId,
+          completed: false,
         })),
-        lastAccessedTimestamp: new Date().toISOString(),
-      });
-      await initialProgress.save();
-  
-      // 4. add enrollment to relevant course
-      await Course.update(
-        { courseId },
-        {
-          $ADD: {
-            enrollments: [{ userId }],
-          },
-        }
-      );
-  
-      res.json({
-        message: "Purchased Course successfully",
-        data: {
-          transaction: newTransaction,
-          courseProgress: initialProgress,
+      })),
+      lastAccessedTimestamp: new Date().toISOString(),
+    });
+    await initialProgress.save();
+
+    // 4. add enrollment to relevant course
+    await Course.update(
+      { courseId },
+      {
+        $ADD: {
+          enrollments: [{ userId }],
         },
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Error creating transaction and enrollment", error });
-    }
+      }
+    );
+
+    res.json({
+      message: "Purchased Course successfully",
+      data: {
+        transaction: newTransaction,
+        courseProgress: initialProgress,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating transaction and enrollment", error });
+  }
 };
--- src/modules/Transactions/transactions.router.ts
+++ src/modules/Transactions/transactions.router.ts
@@ -1,14 +1,12 @@
-
-import { Router } from "express"
+import { Router } from "express";
 import * as TransactionsController from "./controller/transactions";
 const router = Router();
 
-router.post("/stripe/payment-intent", TransactionsController.createStripePaymentIntent);
+router.get("/", TransactionsController.listTransactions);
+router.post(
+  "/stripe/payment-intent",
+  TransactionsController.createStripePaymentIntent
+);
 router.post("/", TransactionsController.createTransaction);
 
-
-
-
-
-
-export default router
+export default router;
