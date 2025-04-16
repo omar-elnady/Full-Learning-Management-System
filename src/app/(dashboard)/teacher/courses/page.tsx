19abe2d1f589dce4ef0ba0ab1ca54c5be6c00f58
omar-elnady <omar.elnadey99@gmail.com>
2025-04-16 23:19:49 +0200

    create all variables

--- src/app/(dashboard)/teacher/courses/page.tsx
+++ src/app/(dashboard)/teacher/courses/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import {
+  useCreateCourseMutation,
+  useDeleteeCourseMutation,
+  useGetCoursesQuery,
+} from "@/state/api";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const CoursesTeacherPage = () => {
-  return <div>
-    
-  </div>;
+  const router = useRouter();
+  const { user } = useUser();
+  const {
+    data: courses,
+    isLoading,
+    isError,
+  } = useGetCoursesQuery({ category: "all" });
+  const [createCourse] = useCreateCourseMutation();
+  const [deleteCourse] = useDeleteeCourseMutation();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  return <div></div>;
 };
 
 export default CoursesTeacherPage;
