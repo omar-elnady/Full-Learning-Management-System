fcdc084d3d9f41bf9f207029f57f41a48e687ee9
omar-elnady <omar.elnadey99@gmail.com>
2025-04-18 15:32:32 +0200

    compelete course user page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,13 +7,20 @@ import Loading from "@/components/Loading";
 import { cn } from "@/lib/utils";
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
+import ChaptersSidebar from "@/components/courses/[courseId]/ChaptersSidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
     const { user, isLoaded } = useUser();
+    const [courseId, setCourseId] = useState<string | null>(null);
+    const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
+        pathname
+    );
     const router = useRouter();
+
+
     useEffect(() => {
-        if (!isLoaded) return; // انتظر حتى يتم تحميل بيانات المستخدم
+        if (!isLoaded) return;
 
         if (!user) {
             router.push("/sign-in");
@@ -29,18 +36,30 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         } else if (!userType) {
             router.push("/setup");
         }
-    }, [isLoaded, user, pathname, router]);
+        if (isCoursePage) {
+            const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+            setCourseId(match ? match[1] : null);
+        } else {
+            setCourseId(null);
+        }
+    }, [isLoaded, user, pathname, router, isCoursePage]);
 
-    if (!isLoaded) return <Loading />; // عرض Loading أثناء تحميل بيانات المستخدم
+    if (!isLoaded) return <Loading />;
 
     return (
         <SidebarProvider>
             <div className="dashboard">
-                {/* Sidebar */}
                 <AppSidebar />
                 <div className="dashboard__content">
-                    <div className={cn("dashboard__main")} style={{ height: "100vh" }}>
-                        <Navbar />
+                    {courseId && <ChaptersSidebar />}
+                    <div
+                        className={cn(
+                            "dashboard__main",
+                            isCoursePage && "dashboard__main--not-course"
+                        )}
+                        style={{ height: "100vh" }}
+                    >
+                        <Navbar isCoursePage={isCoursePage} />
                         <main className="dashboard__body">{children}</main>
                     </div>
                 </div>
