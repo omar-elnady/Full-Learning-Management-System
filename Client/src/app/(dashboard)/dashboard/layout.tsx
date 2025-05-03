e3c00ca775737b119bb6875d3c658a6b2fe7c529
omar-elnady <omar.elnadey99@gmail.com>
2025-05-03 18:09:51 +0300

    style : change style of courses

--- Client/src/app/(dashboard)/dashboard/layout.tsx
+++ Client/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { usePathname, useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import Loading from "@/components/Loading";
@@ -9,72 +9,76 @@ import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
 import ChaptersSidebar from "@/components/courses/[courseId]/ChaptersSidebar";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname();
-    const { user, isLoaded } = useUser();
-    const [courseId, setCourseId] = useState<string | null>(null);
-    const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
-        pathname
-    );
-    const router = useRouter();
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const { user, isLoaded } = useUser();
+  const [courseId, setCourseId] = useState<string | null>(null);
+  const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
+    pathname
+  );
+  const router = useRouter();
 
-    useEffect(() => {
-        const handleNavigation = async () => {
-            if (!isLoaded) return;
+  useEffect(() => {
+    const handleNavigation = async () => {
+      if (!isLoaded) return;
 
-            if (!user) {
-                 router.replace("/sign-in");
-                return;
-            }
+      if (!user) {
+        router.replace("/sign-in");
+        return;
+      }
 
-            const userType = user.publicMetadata?.userType as string;
-            if (pathname === "/dashboard") {
-                if (userType === "teacher") {
-                     router.replace("/dashboard/teacher/courses");
-                } else if (userType === "user") {
-                     router.replace("/dashboard/user/courses");
-                }
-                return;
-            }
-            if (userType === "teacher" && !pathname.includes("/dashboard/teacher")) {
-                 router.replace("/dashboard/teacher/courses");
-                return;
-            }
-            if (userType === "user" && !pathname.includes("/dashboard/user")) {
-                 router.replace("/dashboard/user/courses");
-                return;
-            }
-            if (isCoursePage) {
-                const match = pathname.match(/\/user\/courses\/([^\/]+)/);
-                setCourseId(match ? match[1] : null);
-            } else {
-                setCourseId(null);
-            }
-        };
+      const userType = user.publicMetadata?.userType as string;
+      if (pathname === "/dashboard") {
+        if (userType === "teacher") {
+          router.replace("/dashboard/teacher/courses");
+        } else if (userType === "user") {
+          router.replace("/dashboard/user/courses");
+        }
+        return;
+      }
+      if (userType === "teacher" && !pathname.includes("/dashboard/teacher")) {
+        router.replace("/dashboard/teacher/courses");
+        return;
+      }
+      if (userType === "user" && !pathname.includes("/dashboard/user")) {
+        router.replace("/dashboard/user/courses");
+        return;
+      }
+      if (isCoursePage) {
+        const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+        setCourseId(match ? match[1] : null);
+      } else {
+        setCourseId(null);
+      }
+    };
 
-        handleNavigation();
-    }, [isLoaded, user, pathname, router, isCoursePage]);
+    handleNavigation();
+  }, [isLoaded, user, pathname, router, isCoursePage]);
 
-    if (!isLoaded) return <Loading />;
+  if (!isLoaded) return <Loading />;
 
-    return (
-        <SidebarProvider>
-            <div className="dashboard ">
-                <AppSidebar />
-                <div className="dashboard__content">
-                    {courseId && <ChaptersSidebar />}
-                    <div
-                        className={cn(
-                            "dashboard__main",
-                            isCoursePage && "dashboard__main--not-course"
-                        )}
-                        style={{ height: "100vh" }}
-                    >
-                        <Navbar isCoursePage={isCoursePage} />
-                        <main className="dashboard__body">{children}</main>
-                    </div>
-                </div>
-            </div>
-        </SidebarProvider>
-    );
+  return (
+    <SidebarProvider>
+      <div className="dashboard ">
+        <AppSidebar />
+        <div className="dashboard__content">
+          {courseId && <ChaptersSidebar />}
+          <div
+            className={cn(
+              "dashboard__main",
+              isCoursePage && "dashboard__main--not-course"
+            )}
+            style={{ height: "100vh" }}
+          >
+            <Navbar isCoursePage={isCoursePage} />
+            <main className="dashboard__body">{children}</main>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
 }
--- Client/src/app/(dashboard)/dashboard/user/courses/[courseId]/ChaptersSidebar.tsx
+++ Client/src/app/(dashboard)/dashboard/user/courses/[courseId]/ChaptersSidebar.tsx
@@ -306,3 +306,4 @@ const Chapter = ({
 };
 
 export default ChaptersSidebar;
+
