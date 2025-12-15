572e1354f4ab6056f8f0a8415a39187367534b85
omar-elnady <omar.elnadey99@gmail.com>
2025-12-15 23:57:32 +0200

    feat: Initialize Next.js client application with TypeScript configuration and essential dependencies.

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig}  */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
